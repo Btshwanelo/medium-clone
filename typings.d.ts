47c88cfbea0ad701dfc6263abc8df8839e35167d
Btshwanelo <48760912+Btshwanelo@users.noreply.github.com>
2022-12-17 17:22:06 +0200

    implemented comment functionality

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,4 @@
+
 export interface Post {
   _id: string;
   _createdAt: string;
@@ -6,6 +7,7 @@ export interface Post {
     name: string;
     image: string;
   };
+  comments: Comment[],
   description: string;
   mainImage: {
     asset: {
@@ -16,4 +18,20 @@ export interface Post {
     current: string;
   };
   body: [object]; 
+}
+
+export interface Comment {
+  approved: boolean;
+  comment: comment,
+  email: string;
+  name: string;
+  post: {
+    _ref: string;
+    _type: string;
+  } 
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
 }
